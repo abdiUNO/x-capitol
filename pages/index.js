6e0fc65ecac368db45981bb61ef215e8271977e1
Abdullahi Mahamed <abdullahimahamed0987@gmail.com>
2021-12-07 06:51:24 -0600

    :bug: fix:

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout from '../components/layout';
 import Banner from '../sections/banner';
 import UltimateFeatures from '../sections/ultimate-features';
@@ -7,11 +7,25 @@ import OurServices from '../sections/our-services';
 import AboutUs from '../sections/about-us';
 import Faq from '../sections/faq';
 import { NextSeo } from 'next-seo';
+import Script from 'next/script';
 
 export default function IndexPage() {
+  useEffect(() => {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on('init', (user) => {
+        if (!user) {
+          window.netlifyIdentity.on('login', () => {
+            document.location.href = '/admin/';
+          });
+        }
+      });
+    }
+  }, []);
+
   return (
     <Layout>
       <NextSeo title="X Capitol Home" />
+      <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
       <Banner />
       <AboutUs />
 
