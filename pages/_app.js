81d675f005f0f6ea6143a08b0b4c25b54ac99479
Abdullahi Mahamed <abdullahimahamed0987@gmail.com>
2021-12-11 15:03:45 -0600

    :construction: WIP: work in progress

--- pages/_app.js
+++ pages/_app.js
@@ -73,23 +73,69 @@ function MyApp({ Component, pageProps }) {
             cardType: 'summary_large_image',
           }}
         />
-        <Script
-          id="facebook-pixel"
-          dangerouslySetInnerHTML={{
-            __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', ${fbq.FB_PIXEL_ID});
-          `,
-          }}
+        {/*<Script*/}
+        {/*  id="facebook-pixel"*/}
+        {/*  dangerouslySetInnerHTML={{*/}
+        {/*    __html: `*/}
+        {/*    !function(f,b,e,v,n,t,s)*/}
+        {/*    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?*/}
+        {/*    n.callMethod.apply(n,arguments):n.queue.push(arguments)};*/}
+        {/*    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';*/}
+        {/*    n.queue=[];t=b.createElement(e);t.async=!0;*/}
+        {/*    t.src=v;s=b.getElementsByTagName(e)[0];*/}
+        {/*    s.parentNode.insertBefore(t,s)}(window, document,'script',*/}
+        {/*    'https://connect.facebook.net/en_US/fbevents.js');*/}
+        {/*    fbq('init', ${fbq.FB_PIXEL_ID});*/}
+        {/*  `,*/}
+        {/*  }}*/}
+        {/*/>*/}
+        <Script id="facebook-pixel">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '583792236035545');
+fbq('track', 'PageView')`}
+        </Script>
+        <div id="fb-root"></div>
+        <div id="fb-customer-chat" className="fb-customerchat"></div>
+        <div
+          className="fb-customerchat"
+          attribution="install_email"
+          attribution_version="biz_inbox"
+          page_id="100203922072384"
         />
         <Component {...pageProps} />
+
+        <Script id="fb-chat">
+          {`
+            var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "100203922072384");
+            chatbox.setAttribute("attribution", "biz_inbox");
+          `}
+        </Script>
+        <Script id="fb-chat-sdk">
+          {`
+            window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v12.0'
+              });
+            };
+
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+          `}
+        </Script>
       </ThemeProvider>
     </AnimatePresence>
   );
